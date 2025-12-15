c7fa075f6d3f66fb79b50cbb0e7784f68371e847
kpsssta <kapustaarina21@gmail.com>
2025-12-15 23:36:06 +0300

    program.cs

---
+++ kapusta/program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TextRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
