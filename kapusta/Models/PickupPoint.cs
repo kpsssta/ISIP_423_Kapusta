0781bdc7ef3bc39a6075c403e0b2622a27178385
kpsssta <kapustaarina21@gmail.com>
2025-12-17 23:31:23 +0300

    Create PickupPoint.cs

---
+++ kapusta/Models/PickupPoint.cs
@@ -0,0 +1,8 @@
+namespace pr8.Models;
+
+public class PickupPoint
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+}
